0f0074070f7d8e4f9f93fac581359f23872637a6
vivek singh25 <vivek.pv2813@gmail.com>
2024-02-25 21:19:55 +0530

    lean about ejs and express

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const http = require("http");
+import express from "express";
+import path from "path";
 
- const server=http.createServer((req,res)=>{
-    res.end("<h1>Kyu bhai</h1>")
+const app = express();
+
+app.use(express.static(path.join(path.resolve(),"public"))) 
+
+app.set("view engine","ejs");
+
+app.get("/",(req,res)=>{
+//   res.render("./index",{name:"vivek" ,age :24});
+res.sendFile("index")
 })
 
-server.listen(4000,()=>{
-    console.log("second")
+app.listen(4000,()=>{
+    console.log("asdasd")
 })
